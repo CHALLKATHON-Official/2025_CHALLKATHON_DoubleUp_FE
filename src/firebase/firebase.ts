989657ecc061d298b0942f044103e39a7353225d
fromruiyan <tjwntn@gmail.com>
2025-06-21 22:58:24 +0900

    update README.md and firebase.ts
    GoogleAuthProvider 추가

--- src/firebase/firebase.ts
+++ src/firebase/firebase.ts
@@ -1,6 +1,6 @@
 // src/firebase/firebase.ts
 import { initializeApp } from 'firebase/app';
-import { getAuth } from 'firebase/auth';
+import { getAuth, GoogleAuthProvider } from 'firebase/auth';
 import { getFirestore } from 'firebase/firestore';
 
 const firebaseConfig = {
@@ -15,4 +15,5 @@ const firebaseConfig = {
 const app = initializeApp(firebaseConfig);
 
 export const auth = getAuth(app);
+export const provider = new GoogleAuthProvider();
 export const db = getFirestore(app);
