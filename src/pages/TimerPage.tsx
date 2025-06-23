3183f1bc76e85a6bfcfd68c751183404a6006e06
YunJ <yunj@hufs.ac.kr>
2025-06-23 23:32:14 +0900

    fix:Timer image

--- src/pages/TimerPage.tsx
+++ src/pages/TimerPage.tsx
@@ -1,11 +1,12 @@
 // 타이머 페이지
 import Timer from "../components/Timer";
+import rabbitImg from "../images/studyingRabbit.png";
 
 const TimerPage = () => {
   return(
     <div className="min-h-screen flex flex-col items-center justify-center bg-white">
       {/* 타이머 위에 넣을 이미지 - 수정 가능*/}
-      <img src="src\images\studyingRabbit.png" alt="studyingImage" className="w-24 h-24 mb-6" />
+      <img src={rabbitImg} alt="studyingImage" className="w-24 h-24 mb-6" />
       
       {/* 타이머 컴포넌트 불러옴 */}
       <Timer/>
